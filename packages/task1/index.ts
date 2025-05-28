b1068d153b6a9ba0d1c0464185841e766f3931ed
Amos Aidoo <aidooamos20@gmail.com>
2025-05-28 11:36:38 +0200

    expand to demonstrate bonus tasks

--- packages/task1/index.ts
+++ packages/task1/index.ts
@@ -3,8 +3,7 @@ import { Simulator } from "simulation-lib"
 const simulator = new Simulator({
   seed: 1337,
   interval: 15,
-  totalRuns: 20,
-  timezone: "Europe/Berlin" // saw a slight decrement
+  totalRuns: 5,
 })
 
 const results = simulator.run({
@@ -14,4 +13,52 @@ const results = simulator.run({
   numberOfChargePoints: 20
 })
 
-console.log(results)
+console.log("Results", results)
+
+console.log("---Bonus---")
+// Bonus 1
+// Generate a simple CSV to plot
+console.log("ChargePoint, ConcurrencyFactor")
+for (let chargePoint = 1; chargePoint <= 30; chargePoint++) {
+  const results = simulator.run({
+    arrivalProbabilityMultiplier: 100,
+    chargePointChargeSpeed: 11,
+    consumptionOfCars: 18,
+    numberOfChargePoints: chargePoint
+  })
+  console.log(`${chargePoint}, ${results.concurrencyFactor}`)
+}
+
+// Bonus 2: DST
+const simulatorWithDifferentTimezone = new Simulator({
+  seed: 1337,
+  interval: 15,
+  totalRuns: 5,
+  timezone: "Europe/Berlin" // saw a slight decrement
+})
+
+const results2 = simulatorWithDifferentTimezone.run({
+  arrivalProbabilityMultiplier: 100,
+  chargePointChargeSpeed: 11,
+  consumptionOfCars: 18,
+  numberOfChargePoints: 20
+})
+
+console.log("Results 2", results2)
+
+
+// Bonus 3
+const simulatorWithDifferentSeed = new Simulator({
+  seed: 456789,
+  interval: 15,
+  totalRuns: 5,
+})
+
+const results3 = simulatorWithDifferentSeed.run({
+  arrivalProbabilityMultiplier: 100,
+  chargePointChargeSpeed: 11,
+  consumptionOfCars: 18,
+  numberOfChargePoints: 20
+})
+
+console.log("Results 3", results3)
