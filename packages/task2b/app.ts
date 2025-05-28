9253cbf63332f8c6e3fec47c47855fae905272ad
Amos Aidoo <aidooamos20@gmail.com>
2025-05-28 11:37:20 +0200

    add route for swagger

--- packages/task2b/app.ts
+++ packages/task2b/app.ts
@@ -1,11 +1,17 @@
 import express, { Express } from "express"
 import dotenv from "dotenv"
 import morgan from "morgan"
+import swaggerUi from "swagger-ui-express"
+import YAML from "yamljs"
+import path from "path"
 import controllers from "./controllers"
 import validators from "./validators"
 
 dotenv.config()
 const app: Express = express()
+
+const openapiDocument = YAML.load(path.join(__dirname, "./openapi.yaml"))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiDocument))
 
 app.use(morgan("dev"))
 app.use(express.json())
